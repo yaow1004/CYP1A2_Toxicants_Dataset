f50683015b2cd7edda5e5d33d81571e7a552905f
Yao Wei <yao.wei@xlence.disfeb.unimi.it>
2026-04-08 22:18:44 +0200

    Initial commit

---
+++ inhibitor_clustering/run_inhibitor_clustering_08.py
@@ -0,0 +1,49 @@
+import os
+from rdkit import Chem
+from rdkit.Chem import AllChem, DataStructs
+
+input_file = '/home/users/yao.wei/Desktop/cyp_toxicogenomics/zinc/inhibitor_toxic_dataset.txt'
+
+output_file = '/home/users/yao.wei/Desktop/cyp_toxicogenomics/zinc/inhibitor_toxic_dataset_08.txt'
+
+# Inhibitor/toxicant SMILES list
+inhibitor_smiles_list = [
+    "CC1=NC2=C(N1)C(=O)N(C(=O)N2CC3=CC=CO3)C",        # Furafylline_i1
+    "CC(C1=CC2=CC=CC=C2S1)N(C(=O)N)O", # Zileuton_i2
+    "CS(=O)(=O)C1=CC=C(C=C1)C2=C(C(=O)OC2)C3=CC=CC=C3",    # rofecoxib_i3
+    "COC1=C2C(=CC3=C1OC=C3)C=CC(=O)O2",  # Methoxsalen_i4
+    "C1CCC2=NC3=CC=CC=C3C(=C2C1)N",  # Tacrine_i5
+    "C1=CN=CC=C1C(=O)NN",  # Isoniazid_i6
+    "C[C@H]1COC2=C3N1C=C(C(=O)C3=C(C(=C2N4CCN(CC4)C)F)N)C(=O)O",   # Antofloxacin_i7
+    "COC1=C(C2=C(C[C@H]3C4=CC(=C(C=C4CCN3C2)OC)OC)C=C1)OC",  # Tetrahydropalmatine_i8
+    "C1CC1N2C=C(C(=O)C3=CC(=C(C=C32)N4CCNCC4)F)C(=O)O",   # Ciprofloxacin_i9
+    "COC1=CC(=CC2=C1OCO2)CC=C", # Myristicin_i10
+]
+
+# Convert inhibitor SMILES to fingerprints
+toxic_fps = []
+for smi in inhibitor_smiles_list:
+    mol = Chem.MolFromSmiles(smi)
+    if mol:
+        fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2, 1024)
+        toxic_fps.append(fp)
+
+with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
+    f_out.write("SMILES\tZINC_ID\tSimilarity\n")
+    next(f_in)  # Skip header line
+
+    for i, line in enumerate(f_in):
+        parts = line.strip().split('\t')
+        if len(parts) >= 2:
+            smi, zinc_id = parts[0], parts[1]
+            mol = Chem.MolFromSmiles(smi)
+            if mol:
+                fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2, 1024)
+                max_sim = max(DataStructs.TanimotoSimilarity(fp, tox_fp) for tox_fp in toxic_fps)
+                if max_sim >= 0.8:
+                    f_out.write(f"{smi}\t{zinc_id}\t{max_sim:.3f}\n")
+
+        if i % 10000 == 0:
+            print(f"Processed {i} molecules")
+
+print(f"Finished processing. Output saved to {output_file}")
---
+++ substrate_clustering/run_substrate_clustering_02.py
@@ -0,0 +1,53 @@
+import os
+from rdkit import Chem
+from rdkit.Chem import AllChem, DataStructs
+
+
+input_file = '/home/users/yao.wei/Desktop/cyp_toxicogenomics/zinc/substrate_toxic_dataset.txt'
+
+output_file = '/home/users/yao.wei/Desktop/cyp_toxicogenomics/zinc/substrate_toxic_dataset_02.txt'
+
+# Substrate/toxicant SMILES list
+substrate_smiles_list = [
+    "C1=CC=C2C(=C1)C(=NN=C2NN)NN",        
+    "COC1=C2C3=C(C(=O)CC3)C(=O)OC2=C4[C@@H]5C=CO[C@@H]5OC4=C1",
+    "CCC1=CC=C(C=C1)C(=O)C2=CC(=C(C(=C2)O)O)[N+](=O)[O-]",
+    "CC(=O)NC1=CC=C(C=C1)O",
+    "CCOC1=CC=C(C=C1)NC(=O)C",
+    "C1[C@@H]2[C@@H](C2N)CN1C3=C(C=C4C(=O)C(=CN(C4=N3)C5=C(C=C(C=C5)F)F)C(=O)O)F",
+    "CC(=O)NC1=CC2=C(C=C1)C3=CC=CC=C3C2",
+    "C1=CC=C(C=C1)C2=CC=C(C=C2)N",
+    "C1=CC=C2C3=C4C(=CC2=C1)C=CC5=C4C(=CC=C5)C=C3",
+    "CN1C2=CC(=C3C(=C2N=C1N)C=CC=N3)O",
+    "CC(C)(C1=CC=C(C=C1)O)C2=CC=C(C=C2)O",
+    "C1C[C@H](N(C1)N=O)C2=CN=CC=C2"
+]
+
+# Convert substrate SMILES to fingerprints
+toxic_fps = []
+for smi in substrate_smiles_list:
+    mol = Chem.MolFromSmiles(smi)
+    if mol:
+        fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2, 1024)
+        toxic_fps.append(fp)
+
+
+with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
+    f_out.write("SMILES\tZINC_ID\tSimilarity\n")
+    next(f_in)  # Skip header line
+
+    for i, line in enumerate(f_in):
+        parts = line.strip().split('\t')
+        if len(parts) >= 2:
+            smi, zinc_id = parts[0], parts[1]
+            mol = Chem.MolFromSmiles(smi)
+            if mol:
+                fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2, 1024)
+                max_sim = max(DataStructs.TanimotoSimilarity(fp, tox_fp) for tox_fp in toxic_fps)
+                if max_sim >= 0.4:
+                    f_out.write(f"{smi}\t{zinc_id}\t{max_sim:.3f}\n")
+
+        if i % 10000 == 0:
+            print(f"Processed {i} molecules")
+
+print(f"Finished processing. Output saved to {output_file}")
